339b67fe4c5300ef3a8b010f35229b8bae0d4e31
badalk <badalk@gmail.com>
2017-06-11 09:07:49 -0400

    initial version

---
+++ app.js
@@ -0,0 +1,14 @@
+﻿var express = require('express');
+
+var app = express();
+
+var port = process.env.port || 3000;
+
+app.get('/', function (req, res) {
+
+    res.send('Welcome to Book API');
+});
+
+app.listen(port, function () {
+    console.log('Book API running on port: ' + port);
+});
